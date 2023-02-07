6dc0b55f41ae089e842d7d89de53d8d2626e1939
Shukriii <72143199+Shukriii@users.noreply.github.com>
2023-02-07 20:02:18 +0000

    test

--- MakeupMate/ContentView.swift
+++ MakeupMate/ContentView.swift
@@ -7,13 +7,29 @@
 
 import SwiftUI
 
+//ContentView - name of data structure
+//: View - behaviour of strucutre is a View, and its functionality.
 struct ContentView: View {
-    var body: some View {
+    var body: some View { //body is name of veriable, some View is the type of body
         Text("Hello, world!")
             .padding()
+            
+            
+            
+            
+            
     }
 }
 
+
+
+
+
+
+
+
+
+//connects content previewer
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
