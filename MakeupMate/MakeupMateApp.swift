7754c87fab4e2a44fdc446e620208adf60c7615f
Shukriii <72143199+Shukriii@users.noreply.github.com>
2023-02-07 13:14:56 +0000

    Initial Commit

---
+++ MakeupMate/MakeupMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeupMateApp.swift
+//  MakeupMate
+//
+//  Created by Shukri  Ahmed on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MakeupMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
