cf7bf69ec08fa64eb2e243ffe6bb011eb38672be
Shukriii <shukriahmed12@hotmail.co.uk>
2023-03-07 16:41:14 +0000

    add inventory product UI

---
+++ MakeupMate/InventoryProductView.swift
@@ -0,0 +1,172 @@
+//
+//  InventoryProductView.swift
+//  MakeupMate
+//
+//  Created by Shukri  Ahmed on 07/03/2023.
+//
+
+import SwiftUI
+
+struct InventoryProductView: View {
+    
+    @State private var name = ""
+    @State private var brand = ""
+    @State private var category = ""
+    @State private var shade = ""
+    @State private var stock = ""
+    @State private var expiryDate = Date.now
+    @State private var note = ""
+    
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                topNavigationBar
+                
+                ScrollView{
+                    VStack {
+                        HStack{
+                            Image(systemName: "photo").font(.system(size:120))
+                        }
+                        
+                        Spacer()
+                        
+                        VStack (spacing: 16){
+                            
+                            //NAME
+                            VStack (alignment: .leading) {
+                                if !name.isEmpty {
+                                    Text("Name")
+                                        .font(.subheadline)
+                                    .foregroundColor(.purple)
+                                }
+                                TextField("Name", text: $name)
+                            }
+                            .padding(15)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                            //BRAND
+                            VStack (alignment: .leading) {
+                                if !brand.isEmpty {
+                                    Text("Brand")
+                                        .font(.subheadline)
+                                    .foregroundColor(.purple)
+                                }
+                                TextField("Brand", text: $brand)
+                            }
+                            .padding(15)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                            //CATEGORY
+                            Button {
+                                print("category view appears")
+                            } label:  {
+                                HStack {
+                                    Text("Category")
+                                    Spacer()
+                                    Image(systemName: "chevron.right.circle")
+                                }
+                                .padding(15)
+                                    
+                            }
+                            //.padding(.horizontal, 155)
+                            //.padding(12)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                            //SHADE
+                            VStack (alignment: .leading) {
+                                if !shade.isEmpty {
+                                    Text("Shade")
+                                        .font(.subheadline)
+                                    .foregroundColor(.purple)
+                                }
+                                TextField("Shade", text: $shade)
+                            }
+                            .padding(15)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                            //STOCK
+                            VStack (alignment: .leading) {
+                                if !stock.isEmpty {
+                                    Text("Stock")
+                                        .font(.subheadline)
+                                    .foregroundColor(.purple)
+                                }
+                                TextField("Stock", text: $stock)
+                            }
+                            .padding(15)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                            //EXPIRY DATE, needs to become optional 
+                            VStack {
+                                DatePicker(selection: $expiryDate, in: ...Date.distantFuture, displayedComponents: .date) {
+                                    Text("Expiry Date")
+                                }
+                            }
+                            
+                            //NOTE - fix layout
+                            VStack (alignment: .leading) {
+                                if !note.isEmpty {
+                                    Text("Note")
+                                        .font(.subheadline)
+                                        .foregroundColor(.purple)
+                                        .multilineTextAlignment(.leading)
+                                }
+                                TextField("Note", text: $note)
+                            }
+                            .padding(50)
+                            .background(Color(red: 0.914, green: 0.914, blue: 0.914))
+                            .cornerRadius(5)
+                            
+                        }
+                        .padding(12)
+
+                        HStack{
+                            Button{
+                                print("returns back to inventory view")
+                            } label: {
+                                Image(systemName: "trash")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(.red)
+                            }
+                            
+                            Spacer()
+                                .frame(width: 90)
+                            
+                            Button{
+                                
+                            } label: {
+                                Image(systemName: "checkmark.circle")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(Color("Colour5"))
+                            }
+                       }
+                    }
+                }
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .navigationBarHidden(true)
+        }
+        
+    }
+}
+
+private var topNavigationBar: some View {
+    HStack{
+        Text("Inventory Product")
+            .font(.system(size: 24, weight: .bold))
+        Spacer()
+    }
+    .padding()
+}
+
+struct InventoryProductView_Previews: PreviewProvider {
+    static var previews: some View {
+        InventoryProductView()
+    }
+}
