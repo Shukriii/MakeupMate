7ba0e5a952b863cd75d1716e9b6d7cb399fd88a1
Shukriii <shukriahmed12@hotmail.co.uk>
2023-03-09 13:34:46 +0000

    tidy + added commments

--- MakeupMate/AddInventoryProductView.swift
+++ MakeupMate/AddInventoryProductView.swift
@@ -5,6 +5,16 @@
 //  Created by Shukri  Ahmed on 07/03/2023.
 //
 
+/*
+  InventoryView calls this function when the New Product button is clicked
+  No code has been copied directly
+  Tutorials used -
+  Floating label for text boxes: https://www.youtube.com/watch?v=Sg0rfYL3utI&t=649s&ab_channel=PeterFriese
+  Adding a DatePicker (Calendar): https://www.hackingwithswift.com/quick-start/swiftui/how-to-create-a-date-picker-and-read-values-from-it
+  Storing product into Firestore: https://www.youtube.com/watch?v=dA_Ve-9gizQ&list=PL0dzCUj1L5JEN2aWYFCpqfTBeVHcGZjGw&index=12&ab_channel=LetsBuildThatApp
+ */
+
+
 import SwiftUI
 
 struct AddInventoryProductView: View {
@@ -17,7 +27,6 @@ struct AddInventoryProductView: View {
     @State private var expiryDate = Date.now
     @State private var note = ""
     
-    
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
@@ -26,6 +35,7 @@ struct AddInventoryProductView: View {
                 ScrollView{
                     VStack {
                         HStack{
+                        //TODO: Add photo
                             Image(systemName: "photo").font(.system(size:120))
                         }
                         
@@ -60,6 +70,7 @@ struct AddInventoryProductView: View {
                             .cornerRadius(5)
                             
                             //CATEGORY
+                            //TODO: Add category dropdow or make it a new view - Main Product
                             Button {
                                 print("category view appears")
                             } label:  {
@@ -71,8 +82,6 @@ struct AddInventoryProductView: View {
                                 .padding(15)
                                     
                             }
-                            //.padding(.horizontal, 155)
-                            //.padding(12)
                             .background(Color(red: 0.914, green: 0.914, blue: 0.914))
                             .cornerRadius(5)
                             
@@ -102,14 +111,16 @@ struct AddInventoryProductView: View {
                             .background(Color(red: 0.914, green: 0.914, blue: 0.914))
                             .cornerRadius(5)
                             
-                            //EXPIRY DATE, needs to become optional 
+                            //EXPIRY DATE
+                            //TODO: Let no date be an option
                             VStack {
                                 DatePicker(selection: $expiryDate, in: ...Date.distantFuture, displayedComponents: .date) {
                                     Text("Expiry Date")
                                 }
                             }
                             
-                            //NOTE - fix layout
+                            //NOTE
+                            // TODO: how to make textfields expand
                             VStack (alignment: .leading) {
                                 if !note.isEmpty {
                                     Text("Note")
@@ -118,8 +129,9 @@ struct AddInventoryProductView: View {
                                         .multilineTextAlignment(.center)
                                 }
                                 TextField("Note", text: $note)
+                                    .multilineTextAlignment(.leading)
                             }
-                            .padding(50)
+                            .padding(15)
                             .background(Color(red: 0.914, green: 0.914, blue: 0.914))
                             .cornerRadius(5)
                             
@@ -127,7 +139,6 @@ struct AddInventoryProductView: View {
                         .padding(12)
 
                         HStack{
-                            
                             Button{
                                 storeProduct()
                             } label: {
@@ -141,7 +152,7 @@ struct AddInventoryProductView: View {
             }
             .navigationTitle("Inventory Product")
             .navigationViewStyle(StackNavigationViewStyle())
-            //CANCEL BUTTON
+            //Cancel button
             .toolbar{
                 ToolbarItemGroup(placement: .navigationBarLeading){
                     Button {
@@ -158,11 +169,11 @@ struct AddInventoryProductView: View {
     
     @State var statusMessage = ""
     
+    // Retireves uid from Firebase Auth, then uses it to create collection in Firestore
     private func storeProduct(){
         // set the uid to the uid of the user logged in
         guard let uid = FirebaseManager.shared.auth.currentUser?.uid else { return }
         
-        //database setup
         let document = FirebaseManager.shared.firestore.collection("products")
             .document(uid)
             .collection("inventory")
